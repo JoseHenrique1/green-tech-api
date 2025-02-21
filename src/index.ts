f0a001437f21f13a4d962c082b6bfcd4e2f6d482
josehenriquearaujodasilva2 <josehenriquearaujodasilva2@gmail.com>
2025-02-20 22:23:54 -0300

    feat: add model user and basic setup server

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,9 @@
-let a = 1
-console.log(a);
+import express from "express"
 
+const app = express()
+
+app.get("/", (req, res) => {
+  res.send("Hello World")
+})
+
+app.listen(3000, () => console.log("Server running on port 3000"))
