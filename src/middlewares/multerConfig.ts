cc354114bb72c3eebc99d1e20878b7c13dff159d
GustavoPMarcena <gustavopmarcena@outlook.com>
2025-03-10 11:55:24 -0300

    adicionando multer

---
+++ src/middlewares/multerConfig.ts
@@ -0,0 +1,15 @@
+import { Request, Response, NextFunction } from 'express';
+import multer from "multer";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const storage = multer.diskStorage({
+    destination: function(req, file, cb) {
+      cb(null, `${__dirname}/public`)
+    }, 
+    filename: function(req, file, cb) {
+      cb(null, Date.now() + ".jpg");
+    }
+});
+
+const upload = multer({ storage }).single("file");
