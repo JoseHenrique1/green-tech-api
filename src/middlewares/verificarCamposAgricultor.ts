df23420b45a3aff953d540c056b27089dccedca1
JoseHenrique1 <jh4832184@gmail.com>
2025-03-14 07:48:19 -0300

    feat: adicionando validacoes de email e telefone no agricultor

--- src/middlewares/verificarCamposAgricultor.ts
+++ src/middlewares/verificarCamposAgricultor.ts
@@ -1,10 +1,22 @@
 import { Request, Response, NextFunction } from "express";
+import {validarEmail} from "../utils/validarEmail.ts"
+import {validarTelefone} from "../utils/validarTelefone.ts"
 
 export function verificarCamposAgricultor(req: Request, res: Response, next: NextFunction) {
   const { nome, email, telefone, senha, tamanho, latitude, longitude, tiposCultivo } = req.body;
 
   if (!nome || !email || !telefone || !senha || !tamanho || !latitude || !longitude || !tiposCultivo) {
       res.status(400).json({ error: "Todos os campos são obrigatórios." });
+      return;
+  }
+
+  if (!validarEmail(email)) {
+      res.status(400).json({ error: "E-mail inválido." });
+      return;
+  }
+
+  if (!validarTelefone(telefone)) {
+      res.status(400).json({ error: "Telefone inválido. Use o formato (XX) XXXXX-XXXX." });
       return;
   }
 
@@ -31,6 +43,16 @@ export function verificarCamposAtualizacaoAgricultor(req: Request, res: Response
       res.status(400).json({ error: "Pelo menos um campo deve ser atualizado." });
       return;
   }
+
+  if (!validarEmail(email)) {
+    res.status(400).json({ error: "E-mail inválido." });
+    return;
+}
+
+if (!validarTelefone(telefone)) {
+    res.status(400).json({ error: "Telefone inválido. Use o formato (XX) XXXXX-XXXX." });
+    return;
+}
 
   next();
 }
