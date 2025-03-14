08c5a3899a6be1efe99e396c1581701e10110748
GustavoPMarcena <gustavopmarcena@outlook.com>
2025-03-14 16:28:35 -0300

    remove: linhas

--- src/index.ts
+++ src/index.ts
@@ -8,8 +8,4 @@ app.use(express.json());
 app.use(cors());
 app.use("/", router);
 
-// app.get("/", (req, res) => {
-// 	res.json({ msg: "deu certo" });
-// });
-
 app.listen(3000, () => console.log("Server running on port 3000"));
